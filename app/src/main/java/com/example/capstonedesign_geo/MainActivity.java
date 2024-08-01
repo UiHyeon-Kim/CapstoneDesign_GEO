7a1a131a0d8bf921d3d71e51fb523db01bb4d274
UiHyeon <hyunkim6790@gmail.com>
2024-08-01 23:31:11 +0900

    자바 + 코틀린 언어 혼용 사용 설정 완료 첫 페이지 폰트 및 버튼 변경 필요

--- app/src/main/java/com/example/capstonedesign_geo/MainActivity.java
+++ app/src/main/java/com/example/capstonedesign_geo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.capstonedesign_geo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        EdgeToEdge.enable(this); // 시스템 UI(네비, 상태 바 등)이 콘텐츠에 겹치지 않게 함
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return insets; // UI 요소가 시스템 UI에 가려지지 않게 함
+        });
+
+        Button btnUser = findViewById(R.id.btn_user);
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, User_Registration.class);
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/capstonedesign_geo/User_Registration.kt
@@ -0,0 +1,11 @@
+package com.example.capstonedesign_geo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class User_Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_registration)
+    }
+}
