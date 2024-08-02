e1fc7649ff1469ffb7f9059ee6171137a4f77828
UiHyeon <hyunkim6790@gmail.com>
2024-08-02 21:20:24 +0900

    240802

--- app/src/main/java/com/example/capstonedesign_geo/ui/User_Registration.kt
+++ app/src/main/java/com/example/capstonedesign_geo/ui/User_Registration.kt
@@ -1,11 +1,16 @@
-package com.example.capstonedesign_geo
+package com.example.capstonedesign_geo.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.capstonedesign_geo.R
 
 class User_Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_registration)
+
     }
 }
