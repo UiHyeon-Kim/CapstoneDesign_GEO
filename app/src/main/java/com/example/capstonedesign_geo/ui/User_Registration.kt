e49f4fca32c87b817986660a3ae54bb06697995a
UiHyeon <hyunkim6790@gmail.com>
2024-08-03 16:06:14 +0900

    상태바 투명 설정

--- app/src/main/java/com/example/capstonedesign_geo/ui/User_Registration.kt
+++ app/src/main/java/com/example/capstonedesign_geo/ui/User_Registration.kt
@@ -1,16 +1,17 @@
 package com.example.capstonedesign_geo.ui
 
-import android.os.Build
 import android.os.Bundle
-import android.view.Window
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.capstonedesign_geo.R
+import com.example.capstonedesign_geo.utility.setStatusBarColor
 
 class User_Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_registration)
 
+        // 상태바 색상 변경
+        setStatusBarColor(ContextCompat.getColor(this, R.color.transparent))
     }
 }
---
+++ app/src/main/java/com/example/capstonedesign_geo/utility/Utility.kt
@@ -0,0 +1,19 @@
+package com.example.capstonedesign_geo.utility
+
+import android.app.Activity
+import android.os.Build
+import android.view.View
+import androidx.core.content.ContextCompat
+
+fun Activity.setStatusBarColor(color: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        window.statusBarColor = color
+
+        // 상태바 아이콘 색상 변경
+        if (color == ContextCompat.getColor(this, android.R.color.transparent)){
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            window.decorView.systemUiVisibility = 0
+        }
+    }
+}
