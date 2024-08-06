1c49efb92f01424eeda441567cf682c4e5f2a8a4
Su <96676715+March23Moon@users.noreply.github.com>
2024-08-06 23:38:30 +0900

    오늘은 지도 API Fragment연결한 것에 현재 디바이스 위치 띄울 수 있게 했고, TedPermission이라고 권한 설정 편하게 할 수 있게 다 정의 해놓은 오픈 소스 가져다가 권한설정 사용자에게 물어보는거 넣었습니다.

--- app/src/main/java/com/example/capstonedesign_geo/ui/MainActivity.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.capstonedesign_geo.ui;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.Manifest;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.capstonedesign_geo.R;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,20 +31,32 @@ public class MainActivity extends AppCompatActivity {
             return insets; // UI 요소가 시스템 UI에 가려지지 않게 함
         });
 
-        /*if (isUserPreferencesComplete()) {  // 사용자 선호조 조사가 완료된 경우
-            setContentView(R.layout.activity_main); // 메인 화면으로 이동
-        } else {    // 사용자 선호도 조사가 완료되지 않은 경우 첫 번째 화면으로 이동
-            Intent intent = new Intent(MainActivity.this, UserRegistration.class);
-            startActivity(intent);
-            finish(); // 현재 액티비티 종료
-        }*/
 
         //네이버지도 fragment 인스턴스 적용
         NaverFragment naverFragment = new NaverFragment();
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_zone, naverFragment).commit();
 
+        //권한리스너
+        final PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "권한 설정 완료했습니다.", Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "권한이 거부되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        //권한설정
+        TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("앱을 사용하려면, 접근 권한이 필요합니다.")
+                .setDeniedMessage("권한이 거부되었습니다. 이를 다시 얻으려면, [설정] > [권한]으로 이동하세요.")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
 
 
     }
--- app/src/main/java/com/example/capstonedesign_geo/ui/NaverFragment.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/NaverFragment.java
@@ -4,20 +4,30 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.capstonedesign_geo.R;
+import com.gun0912.tedpermission.PermissionListener;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.List;
 
 
 public class NaverFragment extends Fragment implements OnMapReadyCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource; //위치를 반환하는 구현체
+    private NaverMap naverMap;
     private MapView mapView; //지도 객체 변수
 
     public NaverFragment() { }
@@ -39,13 +49,23 @@ public class NaverFragment extends Fragment implements OnMapReadyCallback {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_naver, container, false);
         mapView = (MapView) rootView.findViewById(R.id.map_fragment);
         mapView.onCreate(savedInstanceState);
+
         mapView.getMapAsync(this);
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE); //런타임 권한 처리를 위해 생성자에 액티비티 객체를 전달하고 권한 요청 코드를 지정해 줘야 한다
 
         return rootView;
     }
 
+
+
     @Override
     public void onMapReady(NaverMap naverMap){
+
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
         //배경 지도 선택
         naverMap.setMapType(NaverMap.MapType.Satellite);
 
