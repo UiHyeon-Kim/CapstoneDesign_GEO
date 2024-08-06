d92e43a60e657a600abf171b45d8727b0b302ed9
Su <96676715+March23Moon@users.noreply.github.com>
2024-08-07 00:42:26 +0900

    충돌 없어졌는지 확인중...22

--- app/src/main/java/com/example/capstonedesign_geo/ui/UserRegHaspet.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/UserRegHaspet.java
@@ -7,16 +7,23 @@ import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserRegHaspet extends AppCompatActivity {
+import com.example.capstonedesign_geo.R;
+import com.example.capstonedesign_geo.utility.StatusBarKt;
+
+public class UserRegHasPet extends AppCompatActivity {
 
     private Button btnNext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_reg_haspet);
+        StatusBarKt.setStatusBarColor(this, getResources().getColor(R.color.mainblue));
+
+        btnNext = findViewById(R.id.btnNext);
 
         btnNext.setOnClickListener(view -> {
-            Intent intent = new Intent(UserRegHaspet.this, UserRegFavorite.class);
+            Intent intent = new Intent(UserRegHasPet.this, UserRegFavorite.class);
             startActivity(intent);
         });
     }
