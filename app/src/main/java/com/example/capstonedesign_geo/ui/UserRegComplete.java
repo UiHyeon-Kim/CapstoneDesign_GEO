eea43df41a862cb49fc9f69f9c0007c54c2adc61
UiHyeon <hyunkim6790@gmail.com>
2024-08-09 21:05:56 +0900

    240809_2105 선호 태그 UI, 데이터베이스 일부 수정

--- app/src/main/java/com/example/capstonedesign_geo/ui/UserRegComplete.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/UserRegComplete.java
@@ -4,18 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.capstonedesign_geo.R;
+import com.example.capstonedesign_geo.data.Model.UserInfo;
+import com.example.capstonedesign_geo.data.Model.UserInfoTemp;
+import com.example.capstonedesign_geo.data.repository.UserRepository;
 import com.example.capstonedesign_geo.utility.StatusBarKt;
 
 public class UserRegComplete extends AppCompatActivity {
 
     private Button btnComplete;
+    private UserRepository userRepository;
+    private UserInfoTemp userInfoTemp;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,18 +28,42 @@ public class UserRegComplete extends AppCompatActivity {
         StatusBarKt.setStatusBarColor(this, getResources().getColor(R.color.mainblue));
 
         btnComplete = findViewById(R.id.btnComplete);
-        btnComplete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(UserRegComplete.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        userRepository = new UserRepository(this);
+        userInfoTemp = getIntent().getParcelableExtra("userInfoTemp");
+
+        btnComplete.setOnClickListener(view -> {
+            UserInfo userInfo = new UserInfo(
+                userInfoTemp.getUserId(),
+                userInfoTemp.getAndroidId(),
+                userInfoTemp.getNickname(),
+                userInfoTemp.isUserType(),
+                userInfoTemp.getAge(),
+                userInfoTemp.getLocation(),
+                userInfoTemp.isHasPet(),
+                userInfoTemp.getFavoriteTags()
+            );
+            userRepository.saveUser(userInfo);
+
+            // saveUserInfo(); // 위 UserInfo 안되면 이것으로 하기
+
+            Intent intent = new Intent(UserRegComplete.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 
     private void saveUserInfo() {
-        SharedPreferences sp = getSharedPreferences("UserPreferences", Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPreferences", Context.MODE_PRIVATE);
 
+        String userId = sharedPreferences.getString("userId", null);
+        String androidId = sharedPreferences.getString("androidId", null);
+        String nickname = sharedPreferences.getString("nickname", null);
+        boolean userType = sharedPreferences.getBoolean("userType", false);
+        int age = sharedPreferences.getInt("age", 0);
+        String location = sharedPreferences.getString("location", null);
+        boolean hasPet = sharedPreferences.getBoolean("hasPet", false);
+        String favoriteTags = sharedPreferences.getString("favoriteTags", null);
+
+        UserInfo userInfo = new UserInfo(userId, androidId, nickname, userType, age, location, hasPet, favoriteTags);
     }
 }
