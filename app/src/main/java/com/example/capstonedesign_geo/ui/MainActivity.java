d9f01377067d725521acc42aabb9d931a6b2f12c
Su <96676715+March23Moon@users.noreply.github.com>
2024-08-27 20:35:58 +0900

    디벨롭push

--- app/src/main/java/com/example/capstonedesign_geo/ui/MainActivity.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/MainActivity.java
@@ -66,8 +66,6 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ChatBotActivity.class);
             startActivity(intent);
         });
-
-
     }
 
     @Override
