34f00d9d6a2f3203c2501f56bfab5a26e884a120
UiHyeon <hyunkim6790@gmail.com>
2024-11-04 02:38:44 +0900

    feat(PlaceDetailActivity): 장소 상세 정보까지 액티비티 이동

--- app/src/main/java/com/example/capstonedesign_geo/ui/PlaceDetailActivity.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/PlaceDetailActivity.java
@@ -1,10 +1,14 @@
 package com.example.capstonedesign_geo.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.capstonedesign_geo.R;
 
 public class PlaceDetailActivity extends AppCompatActivity {
@@ -13,6 +17,27 @@ public class PlaceDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_detailed_info);
 
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String category = intent.getStringExtra("category");
+        String address = intent.getStringExtra("address");
+        String hours = intent.getStringExtra("hours");
+        String imageUrl = intent.getStringExtra("imageUrl");
+
+        TextView titleTextView = findViewById(R.id.title);
+        TextView categoryTextView = findViewById(R.id.category);
+        TextView addressTextView = findViewById(R.id.addr);
+        //TextView hoursTextView = findViewById(R.id.hours);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        titleTextView.setText(title);
+        categoryTextView.setText(category);
+        addressTextView.setText(address);
+        //hoursTextView.setText(hours);
+
+        Glide.with(this)
+                .load(imageUrl)
+                .into(imageView);
 
     }
 }
