d04f7e3d112b2ed022ac250256bad1e3038c1887
UiHyeon <hyunkim6790@gmail.com>
2024-11-04 23:15:17 +0900

    feat(activity_place_detailed_info): ui 수정

--- app/src/main/java/com/example/capstonedesign_geo/ui/PlaceDetailActivity.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/PlaceDetailActivity.java
@@ -10,12 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.capstonedesign_geo.R;
+import com.example.capstonedesign_geo.utility.StatusBarKt;
 
 public class PlaceDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_detailed_info);
+        StatusBarKt.setStatusBarColor(this, getResources().getColor(R.color.mainblue));
 
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
@@ -23,17 +25,30 @@ public class PlaceDetailActivity extends AppCompatActivity {
         String address = intent.getStringExtra("address");
         String hours = intent.getStringExtra("hours");
         String imageUrl = intent.getStringExtra("imageUrl");
+        String tel = intent.getStringExtra("tel");
+        String amenity = intent.getStringExtra("amenity");
+        String shortAddr = intent.getStringExtra("shortAddr");
 
         TextView titleTextView = findViewById(R.id.title);
         TextView categoryTextView = findViewById(R.id.category);
-        TextView addressTextView = findViewById(R.id.addr);
-        //TextView hoursTextView = findViewById(R.id.hours);
+        TextView addressTextView = findViewById(R.id.address);
+        TextView hoursTextView = findViewById(R.id.hours);
         ImageView imageView = findViewById(R.id.header_image);
+        TextView telTextView = findViewById(R.id.tel);
+        TextView amenityTextView = findViewById(R.id.amenity);
+        TextView shortAddrTextView = findViewById(R.id.shortAddress);
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("상단 바 제목");*/
 
         titleTextView.setText(title);
         categoryTextView.setText(category);
         addressTextView.setText(address);
-        //hoursTextView.setText(hours);
+        hoursTextView.setText(hours);
+        telTextView.setText(tel);
+        amenityTextView.setText(amenity);
+        shortAddrTextView.setText(shortAddr);
 
         Glide.with(this)
                 .load(imageUrl)
