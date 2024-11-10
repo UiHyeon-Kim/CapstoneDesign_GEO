530774695573a025bf6d30c8fe9291c3c77be995
UiHyeon <hyunkim6790@gmail.com>
2024-11-11 02:18:44 +0900

    refactor: 기타 수정

--- app/src/main/java/com/example/capstonedesign_geo/ui/UserRegLocation.java
+++ app/src/main/java/com/example/capstonedesign_geo/ui/UserRegLocation.java
@@ -61,6 +61,19 @@ public class UserRegLocation extends AppCompatActivity {
         cityToDistrictMap.put("경기", new ArrayList<>(List.of(select, "고양시", "과천시", "광명시", "광주시", "구리시", "군포시", "김포시", "남양주시", "동두천시", "부천시", "성남시", "수원시", "시흥시", "안산시", "안성시", "안양시", "양주시", "양평군", "여주시", "연천군", "오산시", "용인시", "의왕시", "의정부시", "이천시", "파주시", "평택시", "포천시", "하남시", "화성시")));
         cityToDistrictMap.put("인천", new ArrayList<>(List.of(select, "강화군", "계양구", "남동구", "동구", "미추홀구", "부평구", "서구", "연수구", "옹진군", "중구")));
         cityToDistrictMap.put("강원", new ArrayList<>(List.of(select, "강릉시", "고성군", "동해시", "삼척시", "속초시", "양구군", "양양군", "영월군", "원주시", "인제군", "정선군", "철원군", "춘천시", "태백시", "평창군", "홍천군", "화천군", "횡성군")));
+        cityToDistrictMap.put("충북", new ArrayList<>(List.of(select, "괴산군", "단양군", "보은군", "영동군", "옥천군", "음성군", "제천시", "증평군", "진천군", "청주시", "충주시", "추풍령")));
+        cityToDistrictMap.put("충남", new ArrayList<>(List.of(select, "계룡시", "공주시", "금산군", "논산시", "당진시", "보령시", "부여군", "서산시", "서천군", "아산시", "예산군", "천안시", "청양군", "태안군", "홍성군", "천안시", "청양군", "태안군", "홍성군")));
+        cityToDistrictMap.put("경북", new ArrayList<>(List.of(select, "경산시", "경주시", "고령군", "구미시", "군위군", "김천시", "문경시", "봉화군", "상주시", "성주군", "안동시", "영덕군", "영양군", "영주시", "영천시")));
+        cityToDistrictMap.put("경남", new ArrayList<>(List.of(select, "거제시", "거창군", "고성군", "김해시", "남해군", "밀양시", "사천시", "산청군", "양산시", "의령군", "진주시", "창녕군", "창원시", "통영시", "하동군", "함안군", "함양군", "합천군")));
+        cityToDistrictMap.put("전북", new ArrayList<>(List.of(select, "고창군", "군산시", "김제시", "남원시", "무주군", "부안군", "순창군", "완주군", "익산시", "임실군", "장수군", "전주시", "정읍시", "진안군")));
+        cityToDistrictMap.put("전남", new ArrayList<>(List.of(select, "강진군", "고흥군", "곡성군", "광양시", "구례군", "나주시", "담양군", "목포시", "무안군", "보성군", "순천시", "신안군", "여수시", "영광군", "영암군", "완도군", "장성군", "장흥군", "진도군", "함평군", "해남군", "화순군")));
+        cityToDistrictMap.put("대전", new ArrayList<>(List.of(select, "대덕구", "동구", "서구", "유성구", "중구")));
+        cityToDistrictMap.put("대구", new ArrayList<>(List.of(select, "남구", "달서구", "달성군", "동구", "북구", "서구", "수성구", "중구")));
+        cityToDistrictMap.put("광주", new ArrayList<>(List.of(select, "광산구", "남구", "동구", "북구", "서구")));
+        cityToDistrictMap.put("부산", new ArrayList<>(List.of(select, "강서구", "금정구", "기장군", "남구", "동구", "동래구", "부산진구", "북구", "사상구", "사하구", "서구", "수영구", "연제구", "영도구", "중구", "해운대구")));
+        cityToDistrictMap.put("울산", new ArrayList<>(List.of(select, "남구", "동구", "북구", "울주군", "중구")));
+        cityToDistrictMap.put("세종", new ArrayList<>(List.of(select, "세종시")));
+        cityToDistrictMap.put("제주", new ArrayList<>(List.of(select, "서귀포시", "제주시", "남제주군", "북제주군")));
 
         cityAdapter = new CustomSpinnerAdapter(this, cities); // 커스텀 어댑터 초기화
         citySpinner.setAdapter(cityAdapter); // 스피너에 어댑터 설정
